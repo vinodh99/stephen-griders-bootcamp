5aace9e77236ca87b1ec1d22eb57598cd4814212
vinodh tankasala <vinodh2019@gmail.com>
2019-02-04 00:12:48 -0500

    added reverse stering

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
-
+function reverse(str) {
+	let reverse='';
+	for (let element of str){
+		reverse=element+reverse
+	}
+	return reverse
+}
+	
 module.exports = reverse;
