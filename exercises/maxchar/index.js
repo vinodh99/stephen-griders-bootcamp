f5801f91c5073e0d033af549f131769a295e2d3e
vinodh tankasala <vinodh2019@gmail.com>
2019-02-08 23:11:08 -0500

    added maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  debugger;
+  let max = 0;
+  let maxchar = "";
+  let obj = {};
+  for (let ele of str) {
+    obj[ele] = obj[ele] + 1 || 1;
+  }
+  for (let val in obj) {
+    if (obj[val] > max) {
+      max = obj[val];
+      maxchar = val;
+    }
+  }
+  return maxchar;
+}
 
+maxChar("aaa");
 module.exports = maxChar;
