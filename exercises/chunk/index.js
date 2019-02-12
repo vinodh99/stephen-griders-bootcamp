eb598cb1688c3de44c54dec89bd6c1d8eb579b31
vinodh tankasala <vinodh2019@gmail.com>
2019-02-11 23:38:10 -0500

    added splice for chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,17 +9,25 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-  let chunks = [];
+  //   let chunks = [];
 
-  for (let element of array) {
-    let last = chunks[chunks.length - 1];
-    if (!last || last.length === size) {
-      chunks.push([element]);
-    } else {
-      last.push(element);
-    }
+  //   for (let element of array) {
+  //     let last = chunks[chunks.length - 1];
+  //     if (!last || last.length === size) {
+  //       chunks.push([element]);
+  //     } else {
+  //       last.push(element);
+  //     }
+  //   }
+  //   return chunks;
+
+  let chunks = [];
+  let i = 0;
+  while (i < array.length) {
+    chunks.push(array.slice(i, i + size));
+    i += size;
   }
   return chunks;
 }
-console.log(chunk([1, 2, 3, 4], 2));
+// console.log(chunk([1, 2, 3, 4], 2));
 module.exports = chunk;
