55ae87e3257d6f1e8213004960f048e5ecb51a40
vinodh tankasala <vinodh2019@gmail.com>
2019-02-16 23:42:01 -0500

    added steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -18,22 +18,33 @@
 //       '####'
 
 function steps(n) {
-  let k = n;
-  while (k !== 0) {
-    let str = "";
-    for (let j = 0; j < n - k + 1; j++) {
-      str += "#";
+  for (let i = 0; i < n; i++) {
+    let stairs = "";
+    for (let j = 0; j < n; j++) {
+      if (j <= i) {
+        stairs += "#";
+      } else {
+        stairs += " ";
+      }
+      console.log(stairs);
     }
-    for (let i = 0; i < k - 1; i++) {
-      str += " ";
-    }
-    console.log(str);
-
-    k--;
-    debugger;
-    str = "";
   }
 }
+// function steps(n) {
+//   let k = n;
+//   while (k !== 0) {
+//     let str = "";
+//     for (let j = 0; j < n - k + 1; j++) {
+//       str += "#";
+//     }
+//     for (let i = 0; i < k - 1; i++) {
+//       str += " ";
+//     }
+//     console.log(str);
 
-steps(5);
+//     k--;
+//     debugger;
+//     str = "";
+//   }
+// }
 module.exports = steps;
