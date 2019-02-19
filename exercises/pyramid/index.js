daa144f359c68eda167d015b3804f260fa12fd1e
vinodh tankasala <vinodh2019@gmail.com>
2019-02-18 23:51:00 -0500

    added pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,19 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  let midpoint = Math.floor((2 * n - 1) / 2);
+  for (let row = 0; row < n; row++) {
+    let pyramid = "";
+    for (let column = 0; column < 2 * n - 1; column++) {
+      if (midpoint - row <= column && midpoint + row >= column) {
+        pyramid += "#";
+      } else {
+        pyramid += " ";
+      }
+    }
+    console.log(pyramid);
+  }
+}
 
 module.exports = pyramid;
