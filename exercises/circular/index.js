c8c6b9ddf58989f8feb93fbdc95d998247b00f0c
vinodh tankasala <vinodh2019@gmail.com>
2019-03-14 23:22:40 -0400

    added circular and midpoint

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,17 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+  let slow = list.head;
+  let fast = list.head;
+  while (fast.next && fast.next.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+    if (fast === slow) {
+      return true;
+    }
+  }
+  return false;
+}
 
 module.exports = circular;
