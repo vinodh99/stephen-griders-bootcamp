a250c8c2c278376db0987695530740e6f47505ed
vinodh tankasala <vinodh2019@gmail.com>
2019-03-22 23:11:51 -0400

    added contains

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -17,16 +17,28 @@ class Node {
     this.left = null;
     this.right = null;
   }
-  insert(data) {
+  insert(data, i = 0) {
     if (data < this.data && this.left) {
       this.left.insert(data);
     } else if (data < this.data) {
+      console.log(this.data, this.left, this.right);
       this.left = new Node(data);
     } else if (data > this.data && this.right) {
       this.right.insert(data);
     } else if (data > this.data) {
       this.right = new Node(data);
     }
+  }
+  contains(data) {
+    if (this.data === data) {
+      return this;
+    }
+    if (this.data < data && this.right) {
+      return this.right.contains(data);
+    } else if (this.data > data && this.left) {
+      return this.left.contains(data);
+    }
+    return null;
   }
 }
 
