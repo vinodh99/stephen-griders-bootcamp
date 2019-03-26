e61c9420083397fde90e650e0f961bd6023c7e76
vinodh tankasala <vinodh2019@gmail.com>
2019-03-25 23:59:04 -0400

    added validate

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -6,7 +6,19 @@
 // the parent
 
 function validate(node, min = null, max = null) {
-    if(node.left<node.data && node.right>node.data)
+  if (max !== null && node.data > max) {
+    return false;
+  }
+  if (min !== null && node.data < min) {
+    return false;
+  }
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false;
+  }
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false;
+  }
+  return true;
 }
 
 module.exports = validate;
